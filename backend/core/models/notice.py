6c4a7addfed08e77b6249bf5d88c381532998961
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-04 23:26:51 +1000

    create notice fk user

--- backend/core/models/notice.py
+++ backend/core/models/notice.py
@@ -1,7 +1,12 @@
-from sqlalchemy import String, Text, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Notice(Base):
@@ -15,3 +20,7 @@ class Notice(Base):
         default=False,
         server_default="0",
     )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+    )
+    user: Mapped["User"] = relationship(back_populates="notices")
--- backend/core/models/user.py
+++ backend/core/models/user.py
@@ -1,9 +1,16 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .notice import Notice
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(16), unique=True)
     hashed_password: Mapped[str]
+
+    notices: Mapped[list["Notice"]] = relationship(back_populates="user")
