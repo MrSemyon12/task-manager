056c331fe1c3fa17cac757db1a22c2ed9653fd01
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-07 00:23:08 +1000

    add project mixin

--- backend/core/models/mixins.py
+++ backend/core/models/mixins.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import declared_attr, Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
     from .user import User
+    from .project import Project
 
 
 class UserRelationMixin:
@@ -25,4 +26,25 @@ class UserRelationMixin:
         return relationship(
             "User",
             back_populates=cls._user_back_populates,
+        )
+
+
+class ProjectRelationMixin:
+    _project_id_unique: bool = False
+    _project_id_nullable: bool = False
+    _project_back_populates: str | None = None
+
+    @declared_attr
+    def project_id(cls) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey("projects.id"),
+            unique=cls._project_id_unique,
+            nullable=cls._project_id_nullable,
+        )
+
+    @declared_attr
+    def project(cls) -> Mapped["Project"]:
+        return relationship(
+            "Project",
+            back_populates=cls._project_back_populates,
         )
