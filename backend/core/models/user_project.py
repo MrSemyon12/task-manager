e6e70f4044af2eb9ce4c047b319e0cf5a4ba9748
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-07 15:01:33 +1000

    unique ids

--- backend/core/models/user_project.py
+++ backend/core/models/user_project.py
@@ -3,23 +3,21 @@ from typing import TYPE_CHECKING
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from .base import Base
 from .mixins import UserRelationMixin, ProjectRelationMixin
 
 if TYPE_CHECKING:
     from .role import Role
 
 
-class UserProject(UserRelationMixin, ProjectRelationMixin):
+class UserProject(Base, UserRelationMixin, ProjectRelationMixin):
     __tablename__ = "users_projects"
 
     _user_back_populates = "projects"
-    _user_id_primary_key = True
+    _user_id_unique = True
 
     _project_back_populates = "users"
-    _project_id_primary_key = True
+    _project_id_unique = True
 
-    role_id: Mapped[int] = mapped_column(
-        ForeignKey("roles.id"),
-        primary_key=True,
-    )
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"))
     role: Mapped["Role"] = relationship(back_populates="projects")
