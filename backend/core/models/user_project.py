83fe868346fca26f9f7175a3a0a088371412800d
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-08 00:15:00 +1000

    create user_project table

--- backend/core/models/user_project.py
+++ backend/core/models/user_project.py
@@ -13,11 +13,13 @@ if TYPE_CHECKING:
 class UserProject(Base, UserRelationMixin, ProjectRelationMixin):
     __tablename__ = "users_projects"
 
-    _user_back_populates = "projects"
-    _user_id_unique = True
+    id = None
 
+    _user_id_primary_key = True
+    _user_back_populates = "projects"
+
+    _project_id_primary_key = True
     _project_back_populates = "users"
-    _project_id_unique = True
 
     role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"))
     role: Mapped["Role"] = relationship(back_populates="projects")
