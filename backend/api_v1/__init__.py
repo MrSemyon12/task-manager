362981c9a257d7a18d3c5d67c75142282b1833ed
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-08 22:37:34 +1000

    app title

--- backend/api_v1/__init__.py
+++ backend/api_v1/__init__.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 
-from .priorities.views import router as priorities_router
-from .states.views import router as states_router
-from .roles.views import router as roles_router
 from .auth.views import router as auth_router
+from .roles.views import router as roles_router
+from .states.views import router as states_router
+from .priorities.views import router as priorities_router
 
 
 router = APIRouter()
-router.include_router(router=priorities_router, prefix="/priorities")
-router.include_router(router=states_router, prefix="/states")
-router.include_router(router=roles_router, prefix="/roles")
 router.include_router(router=auth_router, prefix="/auth")
+router.include_router(router=roles_router, prefix="/roles")
+router.include_router(router=states_router, prefix="/states")
+router.include_router(router=priorities_router, prefix="/priorities")
--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from core.config import settings
 from api_v1 import router as router_v1
 
 
-app = FastAPI()
+app = FastAPI(title="Task Manager API")
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
