2d34a3d4cc76c8a92f24b0ea563a048925a0455d
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-08 16:20:14 +1000

    update setting

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,14 +1,21 @@
 from pathlib import Path
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).parent.parent
 
+DB_PATH = BASE_DIR / "db.sqlite3"
 
-class Setting(BaseSettings):
+
+class DbSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = False
+
+
+class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    db_echo: bool = False
+    db: DbSettings = DbSettings()
 
 
-settings = Setting()
+settings = Settings()
