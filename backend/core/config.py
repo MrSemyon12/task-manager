7435ae035092d44075cf30932eb2e079e41418da
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-08 22:08:12 +1000

    Update config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,21 +1,22 @@
 from pathlib import Path
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).parent.parent
 
 DB_PATH = BASE_DIR / "db.sqlite3"
 
 
-class DbSettings(BaseModel):
+class DbSettings(BaseSettings):
     url: str = f"sqlite+aiosqlite:///{DB_PATH}"
     echo: bool = False
 
 
-class AuthSettings(BaseModel):
+class AuthSettings(BaseSettings):
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
-    secret_key: str = "key"
+    secret_key: str = "secret_key"
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 class Settings(BaseSettings):
