2209e1f7c8484f7825a169666680a506a7f9e9d3
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-08 16:07:51 +1000

    add states crud

---
+++ backend/api_v1/states/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.models import State
+
+
+async def get_states(session: AsyncSession) -> list[State]:
+    stmt = select(State).order_by(State.id)
+    result: Result = await session.execute(stmt)
+    states = result.scalars().all()
+    return list(states)
+
+
+async def get_state(session: AsyncSession, state_id: int) -> State | None:
+    return await session.get(State, state_id)
---
+++ backend/api_v1/states/views.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper
+from .dependencies import state_by_id
+from .schemas import State
+from . import crud
+
+router = APIRouter(tags=["States"])
+
+
+@router.get("/", response_model=list[State])
+async def get_states(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.get_states(session=session)
+
+
+@router.get("/{state_id}/", response_model=State)
+async def get_state(
+    state: State = Depends(state_by_id),
+):
+    return state
