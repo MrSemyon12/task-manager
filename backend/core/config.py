c805da37869172de21747266020d8fc6eddb2ff9
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-08 19:35:05 +1000

    update config

--- backend/core/config.py
+++ backend/core/config.py
@@ -12,10 +12,17 @@ class DbSettings(BaseModel):
     echo: bool = False
 
 
+class AuthSettings(BaseModel):
+    algorithm: str = "HS256"
+    access_token_expire_minutes = 30
+    secret_key = "key"
+
+
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
     db: DbSettings = DbSettings()
+    auth: AuthSettings = AuthSettings()
 
 
 settings = Settings()
