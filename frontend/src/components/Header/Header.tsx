3d30905dbc85ea72f80beb4ec4e1bf218a06d1e5
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-20 16:25:37 +1000

    Update Header.tsx

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Layout } from 'antd';
+
+import { Button, Layout } from 'antd';
+import { Avatar, Popover } from 'antd';
 
 const { Header: AntdHeader } = Layout;
 
 export const Header: React.FC = () => {
   return (
-    <AntdHeader style={{ display: 'flex', alignItems: 'center' }}>
+    <AntdHeader
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        background: 'purple',
+      }}
+    >
       <h1 style={{ color: 'white' }}>Task Manager</h1>
+      <Popover
+        trigger='hover'
+        content={
+          <Button type='primary' danger>
+            Logout
+          </Button>
+        }
+      >
+        <Avatar
+          size={48}
+          style={{
+            backgroundColor: '#f56a00',
+            cursor: 'pointer',
+          }}
+          // src='https://xsgames.co/randomusers/avatar.php?g=pixel&key=1'
+        >
+          W
+        </Avatar>
+      </Popover>
     </AntdHeader>
   );
 };
