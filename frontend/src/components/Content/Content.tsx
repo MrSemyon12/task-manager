c129b362454262bd558211e65f1ba65e292757e3
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-20 19:15:05 +1000

    content

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { Layout, theme } from 'antd';
+
+import { Layout } from 'antd';
 
 const { Content: AntdContent } = Layout;
 
 export const Content: React.FC = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
   return (
     <AntdContent
       style={{
         padding: 24,
-        margin: 0,
-        height: 500,
-        background: colorBgContainer,
       }}
     >
       Content
