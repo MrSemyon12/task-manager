c9edf89f0081e232aed9ebfb1b581b3264a90611
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-26 01:35:36 +1000

    add spinner

---
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
+
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+export const Spinner: React.FC = () => <Spin indicator={antIcon} />;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,4 @@
 export { Center } from './Center';
 export { Register, Login } from './Auth';
 export { Content } from './Content';
+export { Spinner } from './Spinner';
