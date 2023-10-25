01197813c6152a64a030e3e15d67945c948cade7
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-26 01:05:42 +1000

    fetching registration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: 'http://localhost:8080',
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api/v1',
 });
+
+export default api;
--- frontend/src/components/Auth/Register.tsx
+++ frontend/src/components/Auth/Register.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { Tooltip, message } from 'antd';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import api from '../../api/axios';
+import axios from 'axios';
 
 import styles from './Auth.module.css';
+
+const REGISTER_URL = '/auth/register';
+
+type User = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+function filterObject(obj: User) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, val]) => val !== '')
+  );
+}
 
 export const Register: React.FC = () => {
   const {
@@ -19,15 +35,17 @@ export const Register: React.FC = () => {
     mode: 'onSubmit',
   });
 
-  type User = {
-    username: string;
-    email: string;
-    password: string;
-  };
-
   const onSubmit = async (data: User) => {
-    console.log(data);
-    message.success('Account created', 5);
+    try {
+      await api.post(REGISTER_URL, filterObject(data));
+      message.success('Account created', 5);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 400) {
+        message.error('Username registered', 5);
+      } else {
+        message.error('Registration failed', 5);
+      }
+    }
   };
 
   return (
