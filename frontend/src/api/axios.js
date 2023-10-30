dc2c573845810ee34fc20de14b4ca5b45377bfce
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-30 23:39:25 +1000

    create private api entity

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+const BASE_URL = 'http://localhost:8000/api/v1';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: BASE_URL,
+});
+
+export const apiPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
 
 export default api;
