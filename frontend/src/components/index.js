00ba0ebdcaa8e4d8afd0681bcdfa10d90eea4611
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-31 01:41:09 +1000

    optimize imports

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,7 @@
-export { Center } from './Center';
-export { Register, Login } from './Auth';
+export { Register, Login, RequireAuth, PersistLogin } from './Auth';
 export { Content } from './Content';
 export { Spinner } from './Spinner';
+export { Center } from './Center';
+export { Header } from './Header';
+export { Footer } from './Footer';
+export { Sider } from './Sider';
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import { Layout } from 'antd';
 
-import { ProjectProvider } from '../contexts/ProjectContext';
-import { Header } from '../components/Header';
-import { Sider } from '../components/Sider';
-import { Content } from '../components/Content';
-import { Footer } from '../components/Footer';
+import { Header, Sider, Content, Footer } from '../components';
 
 export const MainPage: React.FC = () => {
   return (
-    <ProjectProvider>
-      <Layout>
-        <Header />
-        <Layout hasSider style={{ height: 'var(--content-height)' }}>
-          <Sider />
-          <Content />
-        </Layout>
-        <Footer />
+    <Layout>
+      <Header />
+      <Layout hasSider style={{ height: 'var(--content-height)' }}>
+        <Sider />
+        <Content />
       </Layout>
-    </ProjectProvider>
+      <Footer />
+    </Layout>
   );
 };
