024a42170bf86bc3339d6031d23624e3c43f1843
MrSemyon12 <denisenko.box19@gmail.com>
2023-10-31 01:37:40 +1000

    update NotFound page

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button, Result } from 'antd';
+
 import { Center } from '../components';
 
 export const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <Center>
       <Result
         status='404'
         title='404'
         subTitle='Sorry, the page you visited does not exist.'
-        extra={<Button type='primary'>Back Home</Button>}
+        extra={
+          <Button type='primary' onClick={() => navigate('/')}>
+            Back Home
+          </Button>
+        }
       />
     </Center>
   );
