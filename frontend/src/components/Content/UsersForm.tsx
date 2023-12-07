e86132670ed106b0a1c86cac9874dbb3d56cc7ad
MrSemyon12 <denisenko.box19@gmail.com>
2023-12-08 00:25:57 +1000

    handleInvite

--- frontend/src/components/Content/UsersForm.tsx
+++ frontend/src/components/Content/UsersForm.tsx
@@ -1,9 +1,9 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useState, useEffect } from 'react';
 import { Modal, List, Avatar, Select, Button, message } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
 import { jwtDecode } from 'jwt-decode';
 
-import { PROJECT_USERS_URL } from '../../api/urls';
+import { PROJECT_USERS_URL, OUTSIDE_USERS_URL } from '../../api/urls';
 import { useApiPrivate, useProject, useAuth } from '../../hooks';
 import { RoleTag } from '../RoleTag';
 import { User } from '../../types';
@@ -22,6 +22,12 @@ type UsersFormProps = {
   setUsers: Dispatch<SetStateAction<User[]>>;
 };
 
+type UnProjectUser = {
+  id: number;
+  username: string;
+  email: string;
+};
+
 export const UsersForm: React.FC<UsersFormProps> = ({
   open,
   closeForm,
@@ -31,9 +37,35 @@ export const UsersForm: React.FC<UsersFormProps> = ({
 }) => {
   const api = useApiPrivate();
   const { curProject } = useProject();
+  const [allUsers, setAllUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { auth } = useAuth();
   const username = auth ? jwtDecode(auth.access_token).sub : null;
+
+  useEffect(() => {
+    if (!curProject) return;
+    setIsLoading(true);
+
+    api
+      .get(
+        OUTSIDE_USERS_URL.replace(
+          ':projectId',
+          curProject.project.id.toString()
+        )
+      )
+      .then((response) => {
+        setAllUsers(() => [
+          ...users,
+          ...response.data.map((item: UnProjectUser) => ({
+            user: item,
+            role: { id: 4, title: 'outside' },
+          })),
+        ]);
+        setIsLoading(false);
+      })
+      .catch(() => message.error('Service is not available', 5));
+  }, [api, curProject, users]);
 
   const handleOk = () => closeForm();
 
@@ -50,7 +82,8 @@ export const UsersForm: React.FC<UsersFormProps> = ({
         ),
         { data: userId }
       );
-      setUsers((prev) => prev.filter((user) => user.user.id != userId));
+      setUsers((prev) => prev.filter((user) => user.user.id !== userId));
+      setAllUsers((prev) => prev.filter((user) => user.user.id !== userId));
       message.success('User deleted', 5);
     } catch (error) {
       message.error('Service is not available', 5);
@@ -74,7 +107,32 @@ export const UsersForm: React.FC<UsersFormProps> = ({
           return user;
         })
       );
+      setAllUsers((prev) =>
+        prev.map((user: User) => {
+          if (user.user.id === userId) return response.data;
+          return user;
+        })
+      );
       message.success('Role updated', 5);
+    } catch (error) {
+      message.error('Service is not available', 5);
+    }
+  };
+
+  const handleInvite = async (userId: number) => {
+    if (!curProject) return;
+
+    try {
+      const response = await api.post(
+        PROJECT_USERS_URL.replace(
+          ':projectId',
+          curProject.project.id.toString()
+        ),
+        { role_id: 3, user_id: userId }
+      );
+      setUsers((prev) => [...prev, response.data]);
+      setAllUsers((prev) => prev.filter((item) => item.user.id !== userId));
+      message.success('User invited', 5);
     } catch (error) {
       message.error('Service is not available', 5);
     }
@@ -90,7 +148,8 @@ export const UsersForm: React.FC<UsersFormProps> = ({
     >
       <List
         itemLayout='horizontal'
-        dataSource={users}
+        dataSource={allUsers}
+        loading={isLoading}
         renderItem={(item, idx) => (
           <List.Item key={item.user.id}>
             <List.Item.Meta
@@ -106,33 +165,45 @@ export const UsersForm: React.FC<UsersFormProps> = ({
               title={item.user.username}
               description={<RoleTag role={item.role} size='lg' />}
             />
-            {curProject?.role.id === 1 && item.user.username !== username && (
-              <>
-                <Select
-                  defaultValue={item.role.title}
-                  style={{ width: 100, marginRight: 10 }}
-                  options={ROLES}
-                  onChange={(value) => handleChange(value, item.user.id)}
-                />
+            {curProject?.role.id === 1 &&
+              item.user.username !== username &&
+              (item.role.id !== 4 ? (
+                <>
+                  <Select
+                    defaultValue={item.role.title}
+                    style={{ width: 100, marginRight: 10 }}
+                    options={ROLES}
+                    onChange={(value) => handleChange(value, item.user.id)}
+                  />
+                  <Button
+                    danger
+                    icon={<DeleteOutlined style={{ color: '#ff4d4f' }} />}
+                    onClick={() => {
+                      Modal.confirm({
+                        title: 'Delete user?',
+                        content:
+                          'User will no longer have access to the project',
+                        footer: (_, { OkBtn, CancelBtn }) => (
+                          <>
+                            <CancelBtn />
+                            <OkBtn />
+                          </>
+                        ),
+                        onOk: () => handleDelete(item.user.id),
+                      });
+                    }}
+                  />
+                </>
+              ) : (
                 <Button
-                  danger
-                  icon={<DeleteOutlined style={{ color: '#ff4d4f' }} />}
-                  onClick={() => {
-                    Modal.confirm({
-                      title: 'Delete user?',
-                      content: 'User will no longer have access to the project',
-                      footer: (_, { OkBtn, CancelBtn }) => (
-                        <>
-                          <CancelBtn />
-                          <OkBtn />
-                        </>
-                      ),
-                      onOk: () => handleDelete(item.user.id),
-                    });
-                  }}
+                  onClick={() => handleInvite(item.user.id)}
+                  icon={
+                    <PlusOutlined
+                      style={{ fontSize: 22, color: 'var(--color-secondary)' }}
+                    />
+                  }
                 />
-              </>
-            )}
+              ))}
           </List.Item>
         )}
       />
